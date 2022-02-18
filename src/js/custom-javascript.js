fa9167b7a157278bb14e537fd2edeae6124ec856
Iron318i <iron318i@gmail.com>
2022-02-18 09:48:10 +0300

    Header + btns

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,15 +1,14 @@
 +function ($) {
     $('body').append('<div id="toTop" class="btn">&nbsp</div>');
     $(window).scroll(function () {
-	if ($(this).scrollTop() != 0) {
-	    $('#toTop').fadeIn();
-	} else {
-	    $('#toTop').fadeOut();
-	}
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
     });
     $('#toTop').click(function () {
-	$("html, body").animate({scrollTop: 0}, 600);
-	return false;
+        $("html, body").animate({scrollTop: 0}, 600);
+        return false;
     });
-
 }(jQuery);
